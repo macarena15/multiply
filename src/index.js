a60bb12e798e171c24bc21f6647debc57bf2eeb1
Macarena15 <aleksandra.by92@gmail.com>
2019-10-03 15:56:29 +0300

    feat: js multiply code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function multiply(first, second) {
   // your solution
+ //var a = parseInt(first) * parseInt(second);
+ //return a.toLocaleString('fullwide', {useGrouping:false})
+
+  var a = first.split('').reverse();
+  var b = second.split('').reverse();
+
+  var numbers = [];
+
+  for (var i = 0; i < a.length; i++) {
+    for (var j = 0; j < b.length; j++) {
+      var c = a[i] * b[j];
+      numbers[i + j] = (numbers[i + j]) ? numbers[i + j] + c : c;
+    }
+  }
+
+  for (var i = 0; i < numbers.length; i++) {
+    var numb = numbers[i] % 10;
+    var move = Math.floor(numbers[i] / 10);
+    numbers[i] = numb;
+
+    if (numbers[i + 1])
+      numbers[i + 1] += move;
+    else if (move != 0)
+      numbers[i + 1] = move;
+  }
+
+
+  return numbers.reverse().join('');
 }
+
+ 
